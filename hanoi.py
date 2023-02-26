8f3238b0725c250088e4180c8058904a7cd174e8
MariaFernandaOrtega <229757@students.hertie-school.org>
2023-02-26 08:56:20 +0100

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -2,13 +2,20 @@
 """
 Created on Mon Feb 20 08:24:20 2023
 
-@author: Hannah
+@author: Maria Fernanda Ortega
 """
 
-def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
-    
-    #delete the pass and replace with your recursive algorithm
-    pass
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
+    # Recursive Python function to solve tower of hanoi
+    if n == 0:
+        return
+    TowerOfHanoi(n - 1, from_rod, aux_rod, to_rod)
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n - 1, aux_rod, to_rod, from_rod)
 
+# Driver code
+N = 3
 
-TowerOfHanoi(3, 'A', 'C', 'B')  
+# A, C, B are the name of rods
+TowerOfHanoi(N, 'A', 'C', 'B')
+
